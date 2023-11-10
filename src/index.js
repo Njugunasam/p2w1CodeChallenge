80a203a9f8da90d5331b0d4cf16894b284d4af76
Njugunasam <sammynjuguna5826@gmail.com>
2023-11-10 15:57:56 +0300

    added modules to the parent

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './App.css';
+import React, { useState, useEffect } from "react";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
