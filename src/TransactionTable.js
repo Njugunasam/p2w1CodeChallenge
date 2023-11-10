da165fbd1e9367e0b5d6709ae219471e5411fc6e
Njugunasam <sammynjuguna5826@gmail.com>
2023-11-10 16:40:55 +0300

    used the useEffect to fetch the data

--- src/TransactionTable.js
+++ src/TransactionTable.js
@@ -2,5 +2,15 @@ import React, { useState, useEffect } from "react";
 
 function TransactionTable() {
     const [transactions, setTransactions] = useState([]);
+    function TransactionTable() {
+        const [transactions, setTransactions] = useState([]);
+      
+        useEffect(() => {
+          fetch("http://localhost:8001/transactions")
+            .then((response) => response.json())
+            .then((data) => {
+              setTransactions(data);
+            });
+        }, []);
 
 }
