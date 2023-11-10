e2767612f069bad3c5086c9274cb71489e71eab9
Njugunasam <sammynjuguna5826@gmail.com>
2023-11-10 17:00:40 +0300

    create a new transaction object

--- src/TransactionForm.js
+++ src/TransactionForm.js
@@ -8,4 +8,10 @@ function TransactionForm({ addTransaction }) {
 //events handler
   const handleSubmit = (e) => {
     e.preventDefault();
+    const newTransaction = {
+        date,
+        description,
+        category,
+        amount: parseFloat(amount), // Convert amount to a number
+      };
 }
