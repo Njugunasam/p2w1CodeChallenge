8476d62ffaec05863261ae8303a5670a8898beb2
Njugunasam <sammynjuguna5826@gmail.com>
2023-11-13 14:23:36 +0300

    difine TransactionCard function

---
+++ src/TransactionCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './TransactionCard.css'; 
+
+function TransactionCard({ transaction }) {
+  return (
+    <div className="transaction-card">
+      <ul>
+        <li>
+          <span className="label">Description:</span>
+          <span className="value">{transaction.description}</span>
+        </li>
+        <li>
+          <span className="label">Category:</span>
+          <span className="value">{transaction.category}</span>
+        </li>
+        <li>
+          <span className="label">Date:</span>
+          <span className="value">{transaction.date}</span>
+        </li>
+        <li>
+          <span className="label">Amount:</span>
+          <span className="value">{transaction.amount}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default TransactionCard;
