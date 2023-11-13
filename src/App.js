e1f04dac23ad907b2ecf3bed7ffbca50cb8baf17
Njugunasam <sammynjuguna5826@gmail.com>
2023-11-13 14:25:06 +0300

    imported the TransactionCard to the parent

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import TransactionForm from './TransactionForm';
 import TransactionTable from './TransactionTable';
 import SearchBar from './SearchBar';
+import TransactionCard from './TransactionCard';
 
 
 function App() {
