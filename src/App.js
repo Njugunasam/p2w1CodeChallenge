cc57fa3354325054b8bfbf18353d1b3af32a4865
Njugunasam <sammynjuguna5826@gmail.com>
2023-11-13 14:20:30 +0300

    created TransactionCard file

--- src/App.js
+++ src/App.js
@@ -10,29 +10,24 @@ function App() {
   const [transactions, setTransactions] = useState([]);
   const addTransaction = (newTransaction) => {
     setTransactions((prevTransactions) => [...prevTransactions, newTransaction]);
-  };// Declare and initialize state variable
+  };
 
   useEffect(() => {
     fetch("http://localhost:3000/transactions")
       .then(response => response.json())
-      .then(data => setTransactions(data)); // Set the fetched data to the state variable
+      .then(data => setTransactions(data));
   }, []);
 
   return (
     <div className="App">
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <TransactionForm addTransaction={addTransaction} />
-      <TransactionTable transactions={transactions} />
       <div>
         <h2>Transactions</h2>
         <ul>
           {transactions.map(transaction => (
             <li key={transaction.id}>
-              <p>Description: {transaction.description}</p>
-              <p>Category: {transaction.category}</p>
-              <p>Date: {transaction.date}</p>
-              <p>Amount: {transaction.amount}</p>
-              <TransactionTable />
+              <TransactionCard transaction={transaction} /> {/* Use the TransactionCard component */}
             </li>
           ))}
         </ul>
